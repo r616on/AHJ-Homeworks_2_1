ec5d97c7a85c676c15cada5ddf90c60c29cbfc75
Roman Prilepsky <r616on@gmail.com>
2021-06-28 23:12:57 +0300

    done _1_2

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
-import GameField from './GameField/GameField';
-import Goblin from './Goblin/Goblin';
+import GameField from "./GameField/GameField";
+import Goblin from "./Goblin/Goblin";
 
-const play = new GameField('.hole-game');
+export default function demo(value) {
+  return value;
+}
+const play = new GameField(".hole-game");
 play.generate(4);
 
-const goblin = new Goblin('.hole-game');
+const goblin = new Goblin(".hole-game");
 goblin.randomHole();
